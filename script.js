2528f30dffc2e8cd1108f2fdd699963aea26ac31
mokken066 <163156189+mokken066@users.noreply.github.com>
2025-09-27 22:52:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -154,3 +154,24 @@ window.addEventListener('appinstalled', (e) => {
     installSection.style.display = 'none';
 });
 
+// 表示アニメーション
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll(".fade-in");
+
+  const options = {
+    threshold: 0.1,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // 一度表示したら監視を外す
+      }
+    });
+  }, options);
+
+  faders.forEach(fader => {
+    observer.observe(fader);
+  });
+});
